d187146c2e25e3406b4133e7f2fc52b2f2565132
gaelhenri <142238386+gaelhenri@users.noreply.github.com>
2023-11-13 12:11:49 -0300

    index

--- js/questions.js
+++ js/questions.js
@@ -55,6 +55,114 @@ let questions = [
       "Mercúrio e Saturno"
     ]
   },
+  {
+    numb: 6,
+    question:"Quantos biomas há no Brasil?"
+    answer: "6"
+    options: [
+      "5"
+      "4"
+      "6"
+      "7"
+    ]
+  }
+  {
+    numb: 7,
+    question:"Sobre o tema música e seus períodos, é correto afirmar que."
+    answer: "O canto gregoriano e a música sacra se destacam na música medieval"
+    options: [
+      "A polifonia surgiu na música antiga"
+      "os instrumentos de corda surgiram na música moderna"
+      "O canto gregoriano e a música sacra se destacam na música medieval"
+    ]
+  }
+  {
+    numb: 8,
+    question:"Para Tales de Mileto o arché era?"
+    answer: "água"
+    options: [
+      "fogo"
+      "ar"
+      "água"
+    ]
+  }
+  {
+  numb: 9,
+  question:"Quem foi a primeira pessoa a viajar no Espaço?"
+  answer:"Yuri Gagarin"
+  options[
+     "Yuri Gagarin"
+     "A cadela Laika"
+     "Neil Armstrong"
+     "Marcos Pontes "
+  ]
+  }
+  {
+    numb: 10,
+    question:"Onde se localiza Machu Picchu?"
+    answer:"Peru"
+    options[
+       "Colômbia"
+       "Peru"
+       "China"
+       "Bolívia"
+    ]
+    }
+    {
+      numb: 11,
+      question:"Quantos ossos temos no nosso corpo?"
+      answer:"206"
+      options[
+        "126"
+        "206"
+        "18"
+        "300"
+      ]
+    }
+    {
+      numb: 12,
+      question:"Qual o maior planeta do sistema solar?"
+      answer:"Júpiter"
+      options[
+        "Terra"
+        "Plutão"
+        "Júpiter"
+        "Marte"
+      ]
+    }
+    {
+      numb: 13,
+      question:"Quantos continentes existem?"
+      answer:"6"
+      options[
+        "7"
+        "6"
+        "8"
+        "9"
+      ]
+    }
+    {
+      numb: 14,
+      question:"Qual o menoror planeta do sistema solar?"
+      answer:"Plutão"
+      options[
+        "Terra"
+        "Plutão"
+        "Júpiter"
+        "Marte"
+      ]
+    }
+    {
+      numb: 15,
+      question:"Qual é a moeda do Brasil?"
+      answer:"Júpiter"
+      options[
+        "real"
+        "euro"
+        "dólar"
+        
+      ]
+    }
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
